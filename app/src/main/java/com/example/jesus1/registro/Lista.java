1009cfc8e6f2503b9393be0c006291f24cb072e5
Seeker <susej013@gmail.com>
2016-01-18 18:00:08 +0100

    Funciona todo, salvo añadir uno nuevo. Ligero error cuando se sale de la pantalla NuevoObjeto si no es a través del botón enviar.

--- app/src/main/java/com/example/jesus1/registro/Lista.java
+++ app/src/main/java/com/example/jesus1/registro/Lista.java
@@ -29,9 +29,15 @@ public class Lista extends AppCompatActivity implements Serializable {
 
     private ListView vista_objetos;
 
-    ArrayAdapter<Objeto> adapterListaObjetos;
+    //ArrayAdapter<Objeto> adapterListaObjetos;
 
     ArrayList<Objeto> lista_objetos = new ArrayList<Objeto>();
+
+
+
+    private final int CODE1 = 1;
+    private final int CODE2 = 2;
+    int post;
 
     // Necesario para la clase AdaptadorObjeto
     private AdaptadorObjeto adaptadorObjeto;
@@ -48,12 +54,15 @@ public class Lista extends AppCompatActivity implements Serializable {
         vista_objetos = (ListView) findViewById(R.id.vista_objetos);
         // Se asemeja a la construccion de la clase AdaptadorObjeto
 
+
+
         adaptadorObjeto = new AdaptadorObjeto(this);
 
-        //vista_objetos.setAdapter(adaptadorObjeto);
+        vista_objetos.setAdapter(adaptadorObjeto);
 
-        adapterListaObjetos = new ArrayAdapter<Objeto>(this,android.R.layout.simple_dropdown_item_1line,lista_objetos);
-        vista_objetos.setAdapter(adapterListaObjetos);
+        // adapterListaObjetos = new ArrayAdapter<Objeto>(this,android.R.layout.simple_dropdown_item_1line,lista_objetos);
+        //vista_objetos.setAdapter(adapterListaObjetos);
+
 
 
         registerForContextMenu(vista_objetos);
@@ -69,14 +78,23 @@ public class Lista extends AppCompatActivity implements Serializable {
     @Override
     public boolean onContextItemSelected(MenuItem item) {
         final AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-        switch (item.getItemId()){
+
+
+        switch (item.getItemId()) {
 
             case R.id.opcEditar:
 
+                // No funciona de esta forma.
                 Objeto o = lista_objetos.get(info.position);
-                Intent i = new Intent(Lista.this,NuevoObjeto.class);
-                i.putExtra("EditarObjeto", o);
-                startActivityForResult(i, 1);
+                Intent i = new Intent(Lista.this, NuevoObjeto.class);
+                i.putExtra("emisor", o.getEmisor());
+                i.putExtra("destinatario", o.getDestinatario());
+                i.putExtra("urgencia", String.valueOf(o.getNivel_urgencia()));
+                i.putExtra("nota", o.getTexto());
+
+                //i.putExtra("EditarObjeto", o);
+                post = info.position;
+                startActivityForResult(i, CODE1);
                 break;
             case R.id.opcBorrar:
                 AlertDialog.Builder alerta = new AlertDialog.Builder(this);
@@ -87,8 +105,9 @@ public class Lista extends AppCompatActivity implements Serializable {
                     public void onClick(DialogInterface dialog, int which) {
 
                         lista_objetos.remove(info.position);
+                        //lista_objetos.remove(o);
                         adaptadorObjeto.notifyDataSetChanged();
-                        Toast.makeText(getApplicationContext(),"Borrado con éxito",Toast.LENGTH_LONG).show();
+
                     }
                 });
                 AlertDialog dialogo = alerta.create();
@@ -101,86 +120,87 @@ public class Lista extends AppCompatActivity implements Serializable {
 
     private ArrayList<Objeto> CargarLista() {
         ArrayList<Objeto> aux = new ArrayList<Objeto>();
-        aux.add(new Objeto("Jrublgo","Javier Ruano",3,"Este es el proyecto de Android"));
-        aux.add(new Objeto("Prueba","Prueba","ESTE ES EL TEXTO"));
-        aux.add(new Objeto("Yo","A mi mismo",2,"Acabar este proyecto"));
-        aux.add(new Objeto("Yo","Jrublgo",1,"Mensaje a otro usuario"));
-        aux.add(new Objeto("Yo","Javier Ruano","Quisiera revisar mi examen"));
-        aux.add(new Objeto("Root","Prueba","Me estoy quedando sin ideas"));
-        aux.add(new Objeto("Admin","yo",3,"Estás suspendido"));
+        aux.add(new Objeto("Jrublgo", "Javier Ruano", 3, "Este es el proyecto de Android"));
+        aux.add(new Objeto("Prueba", "Prueba", "ESTE ES EL TEXTO"));
+        aux.add(new Objeto("Yo", "A mi mismo", 2, "Acabar este proyecto"));
+        aux.add(new Objeto("Yo", "Jrublgo", 1, "Mensaje a otro usuario"));
+        aux.add(new Objeto("Yo", "Javier Ruano", "Quisiera revisar mi examen"));
+        aux.add(new Objeto("Root", "Prueba", "Me estoy quedando sin ideas"));
+        aux.add(new Objeto("Admin", "yo", 3, "Estás suspendido"));
         aux.add(new Objeto("Root", "A mi mismo", "No vale la pena"));
-        aux.add(new Objeto("Profesor","Alumno","Tienes un 0"));
-        aux.add(new Objeto("Jrublgo","Prueba",2,"Mensaje de poca importancia"));
+        aux.add(new Objeto("Profesor", "Alumno", "Tienes un 0"));
+        aux.add(new Objeto("Jrublgo", "Prueba", 2, "Mensaje de poca importancia"));
         aux.add(new Objeto("Profesor", "Yo", 2, "Hablando conmigo mismo"));
         aux.add(new Objeto("Alumno", "Una persona", 1, "ESTE ES EL TEXTO"));
-        return  aux;
+        return aux;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Toast.makeText(Lista.this, "Nuevo elemento añadido", Toast.LENGTH_SHORT).show();
-        if (requestCode == 1) {
-            lista_objetos.add((Objeto) data.getSerializableExtra("NuevoObjeto"));
+        //ista_objetos.add((Objeto) data.getSerializableExtra("NuevoObjeto"));
+        String emisor = data.getStringExtra("emisor");
+        String destinatario = data.getStringExtra("destinatario");
+        int nota = (data.getIntExtra("nota", 0));
+        String mensaje = data.getStringExtra("mensaje");
+        Objeto nO = new Objeto(emisor, destinatario, nota, mensaje);
+
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == CODE1) {
+                lista_objetos.set(post, nO);
+            } else if (requestCode == CODE2) {
+                lista_objetos.add(nO);
+            }
             adaptadorObjeto.notifyDataSetChanged();
         }
+
     }
 
     private class AdaptadorObjeto extends ArrayAdapter {
 
-        Activity actividad;
+        Activity context;
 
-        public AdaptadorObjeto(Activity actividad) {
-            // Constructor con solo el contexto. Confunde el context context con el activity context
-            super(actividad,R.layout.fila_objetos,lista_objetos);
-            // En el super le pasamos la actividad, el layout que vamos a inflar y mostrar en
-            // el ListView y la lista de objetos que está ligada al ListView a través del adaptador
-            this.actividad = actividad;
+        public AdaptadorObjeto(Activity context) {
+            super(context, R.layout.fila_objetos, lista_objetos);
+            // Layout y lista de objetos
+            this.context = context;
         }
-        // Obtener vista
+
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-           // Borramos el super de este
-
             View item = convertView;
-            // Se puede usar directamente el convertView
 
             ViewHolder holder;
-            // Documentar
 
             if (item == null) {
-                LayoutInflater inflater = actividad.getLayoutInflater();
-
+                LayoutInflater inflater = context.getLayoutInflater();
                 item = inflater.inflate(R.layout.fila_objetos, null);
 
-
                 holder = new ViewHolder();
-
-                holder.txt_Destinatario = (TextView) item.findViewById(R.id.txt_destinatario);
-                holder.txt_Emisor = (TextView) item.findViewById(R.id.txt_emisor);
-                holder.txt_urgencia = (TextView) item.findViewById(R.id.txt_grado);
                 holder.txt_nota = (TextView) item.findViewById(R.id.txt_nota);
+                holder.txt_grado = (TextView) item.findViewById(R.id.txt_grado);
+                holder.txt_destinatario = (TextView) item.findViewById(R.id.txt_destinatario);
+                holder.txt_emisor = (TextView) item.findViewById(R.id.txt_emisor);
 
                 item.setTag(holder);
             } else {
                 holder = (ViewHolder) item.getTag();
             }
 
-            holder.txt_Destinatario.setText(lista_objetos.get(position).getDestinatario());
-            holder.txt_Emisor.setText(lista_objetos.get(position).getEmisor());
-            holder.txt_urgencia.setText(lista_objetos.get(position).getNivel_urgencia());
+            holder.txt_emisor.setText(lista_objetos.get(position).getEmisor());
+            holder.txt_destinatario.setText(lista_objetos.get(position).getDestinatario());
+            holder.txt_grado.setText(String.valueOf(lista_objetos.get(position).getNivel_urgencia()));
             holder.txt_nota.setText(lista_objetos.get(position).getTexto());
-
             return item;
         }
     }
-
     static class ViewHolder {
-        private TextView txt_Emisor;
-        private TextView txt_Destinatario;
-        private TextView txt_urgencia;
-        private TextView txt_nota;
-
+        TextView txt_emisor;
+        TextView txt_destinatario;
+        TextView txt_grado;
+        TextView txt_nota;
 
     }
-        }
+}
 
