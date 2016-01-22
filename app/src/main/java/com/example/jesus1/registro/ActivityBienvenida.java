cc1d8d7db4a3ea7b1fa81313050539ba09075469
Seeker <susej013@gmail.com>
2016-01-22 17:52:49 +0100

    Version 2.0 Ahora con Pantalla de Inicio

---
+++ app/src/main/java/com/example/jesus1/registro/ActivityBienvenida.java
@@ -0,0 +1,36 @@
+package com.example.jesus1.registro;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class ActivityBienvenida extends AppCompatActivity {
+
+ private static final long SPLASH_SCREEN_DELAY = 3500;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_bienvenida);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent i = new Intent(ActivityBienvenida.this, Registro.class);
+                startActivity(i);
+                finish();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+    }
+}
